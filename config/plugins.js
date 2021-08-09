52870564a4f8db4d717693aceeb32d9830b0c1a9
Stefano Frontini <stefanofrontini75@gmail.com>
2021-08-09 13:02:43 +0200

    nodemailer - documentation

--- config/plugins.js
+++ config/plugins.js
@@ -7,4 +7,20 @@ module.exports = ({ env }) => ({
       api_secret: env("CLOUDINARY_SECRET"),
     },
   },
+  email: {
+    provider: "nodemailer",
+    providerOptions: {
+      host: env("ZOHO_SMTP"),
+      port: env("SMTP_PORT", 587),
+      auth: {
+        user: env("ZOHO_USER"),
+        pass: env("ZOHO_PSW"),
+      },
+      // ... any custom nodemailer options
+    },
+    settings: {
+      defaultFrom: "info@pilloledienergia.com",
+      defaultReplyTo: "info@pilloledienergia.com",
+    },
+  },
 });
