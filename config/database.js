7e926d3eb48337226a2b45c3a382a836fa1fb6e1
Stefano Frontini <stefanofrontini75@gmail.com>
2022-11-09 12:20:08 +0100

    ssl

--- config/database.js
+++ config/database.js
@@ -10,13 +10,13 @@ module.exports = ({ env }) => ({
         database: env("DATABASE_NAME"),
         username: env("DATABASE_USERNAME"),
         password: env("DATABASE_PASSWORD"),
-        ssl: {
-          rejectUnauthorized: false,
-        },
+        // ssl: {
+        // rejectUnauthorized: false,
+        // },
       },
-      options: {
-        ssl: true,
-      },
+      // options: {
+      // ssl: true,
+      // },
     },
   },
 });
